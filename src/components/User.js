a1fdfba8ee71fa33cbdbc1f2fa195800e91cb957
Karl <kparelo@gmail.com>
2019-11-08 22:30:04 +0200

    task 1

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(firstname,lastname,birthdate, facaulty,gpa){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.brithday=birthdate;
+        this.facaulty=facaulty;
+        this.gpa=gpa;
+    }
+}
